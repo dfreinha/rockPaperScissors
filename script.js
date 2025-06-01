3324ab206d40813d80a573c705031fb300da5ff8
Dan Reinhardt <58948625+dfreinha@users.noreply.github.com>
2025-05-31 17:53:32 -0700

    add function to get computer choice

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice () { // function will return a string of either "rock" "paper" "scissors"
+    console.log(Math.random())
+
+}
+
+function getHumanChoice () {
+
+    console.log("placeholder")
+}
+getComputerChoice()
