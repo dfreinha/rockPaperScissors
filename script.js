e8dd8997bd9f50ad42b74c6eb8724cf8eb423070
Dan Reinhardt <58948625+dfreinha@users.noreply.github.com>
2025-06-09 13:32:41 -0700

    clean up

--- script.js
+++ script.js
@@ -58,53 +58,17 @@ function startGame() {
 
 function displayChoice(choice) {
     winner = findWhoWon(choice, computerChoice);
+
+    // update score
     if (winner === "Human") {
         humanScore++;
     } else if (winner === "Computer") {
         computerScore++;
     }
-    
+    // dispaly choices and who won
     alert(`You chose: ${choice}\nThe CPU chose: ${computerChoice}\n${winner === "Tie" ? "It was a TIE!" : `The ${winner} has won!`}`);
     document.getElementById("popup").style.display = "none";
     document.getElementById("scoreDisplay").innerText = `Score: Human - ${humanScore} | Computer - ${computerScore}`;
 
 }
 
-/*
-
-let humanScore = 0;
-let computerScore = 0;
-let winner;
-let messageToUser;
-
-while (winner != "Quit") { // main loop
-
-    let humanChoice = getHumanChoice();
-    let computerChoice = (getComputerChoice());
-
-    winner = findWhoWon(humanChoice,computerChoice,winner);
-
-    if (winner == "Human"){
-            humanScore++
-        }
-        else if (winner =="Computer"){
-            computerScore++
-        }
-    
-    if (winner == "Human" || winner == "Computer"){
-    
-        messageToUser = "You selected " + humanChoice + " The computer selected " + computerChoice + "   " + winner + " has won, the score is now Human: " + humanScore + " Computer: " + computerScore;
-        console.log(messageToUser)
-    }       
-    else if (winner == "Tie"){
-        messageToUser = "You selected " + humanChoice + " The computer selected " + computerChoice + "  " + " it was a Tie!! " + "The score is now Human: " + humanScore + " Computer: " + computerScore;
-        console.log(messageToUser)
-    }
-
-    else{
-        messageToUser = "Thanks for playing. Final score was Human: " + humanScore + " Computer: " + computerScore;
-        console.log(messageToUser)
-    }
-}
-
-*/
